1cd24db4d0e46e0ea29362b8569ff6aa38d20e92
Brenden Forward <bforward@umich.edu>
2023-12-17 23:58:47 -0500

    Fix SQL parsing and formatting

--- schema.sql
+++ schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE Users (
-	id			INTEGER			NOT NULL	PRIMARY KEY		AUTOINCREMENT,
-	discord_id	TEXT			NOT NULL	UNIQUE,
-	created_at	TEXT			NOT NULL					DEFAULT CURRENT_TIMESTAMP
+  id			    INTEGER		    NOT NULL	  PRIMARY KEY		AUTOINCREMENT,
+  discord_id	TEXT			    NOT NULL	  UNIQUE,
+  created_at	TEXT			    NOT NULL					        DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Transactions (
-	id			INTEGER			NOT NULL	PRIMARY KEY		AUTOINCREMENT,
-	user_from	INTEGER			NOT NULL,
-	user_to		INTEGER			NOT NULL,
-	amount		DECIMAL(10,2)	NOT NULL,
-	reason		TEXT			NOT NULL,
-	created_at	TEXT			NOT NULL					DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (user_from) REFERENCES Users (id),
-	FOREIGN KEY (user_to)	REFERENCES Users (id)
+  id			    INTEGER			  NOT NULL	  PRIMARY KEY		AUTOINCREMENT,
+  user_from	  INTEGER			  NOT NULL,
+  user_to		  INTEGER			  NOT NULL,
+  amount		  DECIMAL(10,2)	NOT NULL,
+  reason		  TEXT			    NOT NULL,
+  created_at	TEXT			    NOT NULL		              DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_from) REFERENCES Users (id),
+  FOREIGN KEY (user_to)   REFERENCES Users (id)
 );
--- scripts/create_db.ts
+++ scripts/create_db.ts
@@ -21,10 +21,61 @@ if (!(await schemaFile.exists())) {
 const db = new Database(dbFile.name!, { create: true });
 const schema = await schemaFile.text();
 
-const statements = schema.split(/(?<!['"])((?:'[^']*'|[^';])*)(?:--.*)?;/g);
+function splitSQLStatements(sql: string): string[] {
+  const results: string[] = [];
+
+  const enum State {
+    DEFAULT,
+    IN_STRING,
+    IN_SINGLE_LINE_COMMENT,
+    IN_MULTI_LINE_COMMENT,
+  }
+
+  let currentStatement = "";
+  let state: State;
+
+  let i = 0;
+  state = State.DEFAULT;
+
+  while (i < sql.length) {
+    const char = sql[i];
+    const la = sql.slice(i, i + 2);
+
+    switch (state) {
+      case State.DEFAULT:
+        if (char == "'") state = State.IN_STRING;
+        else if (la == "--") state = State.IN_SINGLE_LINE_COMMENT;
+        else if (la == "/*") state = State.IN_MULTI_LINE_COMMENT;
+        else if (char == ";") results.push(currentStatement.trim()), (currentStatement = "");
+        else if (/[\s\n]{2}/m.test(la) || (/[\s\n]/m.test(char) && /[\s\n]/m.test(currentStatement.at(-1) ?? "")))
+          void 0; // remove back-to-back whitespace
+        else if (/[\s\n]/m.test(char)) currentStatement += " ";
+        else currentStatement += char;
+        break;
+      case State.IN_STRING:
+        if (char == "'" && la != "''") state = State.DEFAULT;
+        currentStatement += char;
+        break;
+      case State.IN_SINGLE_LINE_COMMENT:
+        if (char == "\n") state = State.DEFAULT;
+        break;
+      case State.IN_MULTI_LINE_COMMENT:
+        if (la == "*/") (state = State.DEFAULT), i++;
+        break;
+    }
+
+    i++;
+  }
+
+  if (currentStatement.trim()) results.push(currentStatement.trim());
+
+  return results;
+}
+
+const statements = splitSQLStatements(schema);
 
 for (const stmt of statements) {
-  if (!stmt.trim()) continue;
+  console.log(stmt);
   db.run(stmt);
 }
 
